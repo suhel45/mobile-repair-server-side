982ec896112bf926322b393ab45491b0247405c6
suhel45 <suhel.ict45@gmail.com>
2022-11-09 02:57:45 +0600

    data added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 
 
@@ -14,7 +15,28 @@ app.use(express.json());
 app.get('/',(req,res)=>{
     res.send('hello node')
 })
+//user: mobiledb
+//password: YuBXDeCkhzDJfXNM
 
+const uri = "mongodb+srv://mobiledb:YuBXDeCkhzDJfXNM@cluster0.dwtnipt.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const userCollection  = client.db("mobile").collection("services")
+        app.get('/service',async(req,res)=>{
+            const query = {};
+            const cursor = userCollection.find(query);
+            const service = await cursor.toArray();
+            res.send(service);
+        })
+    }
+    finally{
+
+    }
+
+}
+run().catch(e=>console.log(e))
 
 app.listen(port,()=>{
     console.log(`server is running at port:${port}`)
